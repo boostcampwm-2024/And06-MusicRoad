022d1d47904b42c01c827be07aba8f825d83cf5b
miller198 <miller198@naver.com>
2025-02-11 18:39:35 +0900

    [refactor] 유저정보 화면 로그인 버튼 삭제

--- app/src/main/java/com/squirtles/musicroad/userinfo/screen/UserInfoScreen.kt
+++ app/src/main/java/com/squirtles/musicroad/userinfo/screen/UserInfoScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -53,7 +52,6 @@ import com.squirtles.musicroad.account.GoogleId
 import com.squirtles.musicroad.common.Constants.COLOR_STOPS
 import com.squirtles.musicroad.common.DefaultTopAppBar
 import com.squirtles.musicroad.common.DialogTextButton
-import com.squirtles.musicroad.common.GoogleSignInButton
 import com.squirtles.musicroad.common.HorizontalSpacer
 import com.squirtles.musicroad.common.MessageAlertDialog
 import com.squirtles.musicroad.common.VerticalSpacer
@@ -65,7 +63,7 @@ import com.squirtles.musicroad.userinfo.components.UserInfoMenus
 
 @Composable
 fun UserInfoScreen(
-    userId: String?,
+    userId: String,
     onBackClick: () -> Unit,
     onBackToMapClick: () -> Unit,
     onFavoritePicksClick: (String) -> Unit,
@@ -115,146 +113,129 @@ fun UserInfoScreen(
                 .background(Brush.verticalGradient(colorStops = COLOR_STOPS))
                 .padding(innerPadding),
         ) {
-            if (userId == null) {
-                GoogleSignInButton(
-                    onClick = {
-                        GoogleId(context).signIn(
-                            onSuccess = { credential ->
-                                accountViewModel.signIn(credential)
-                                onBackToMapClick()
-                            }
-                        )
-                    },
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
+                    .padding(bottom = 96.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                VerticalSpacer(16)
+
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(user.userProfileImage)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = stringResource(R.string.user_info_profile_image),
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 20.dp)
-                        .align(Alignment.TopCenter)
+                        .size(180.dp)
+                        .clip(CircleShape),
+                    placeholder = painterResource(R.drawable.img_user_default_profile),
+                    error = painterResource(R.drawable.img_user_default_profile),
+                    contentScale = ContentScale.Crop,
                 )
-            } else {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .verticalScroll(scrollState)
-                        .padding(bottom = 96.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    VerticalSpacer(16)
 
-                    AsyncImage(
-                        model = ImageRequest.Builder(LocalContext.current)
-                            .data(user.userProfileImage)
-                            .crossfade(true)
-                            .build(),
-                        contentDescription = stringResource(R.string.user_info_profile_image),
-                        modifier = Modifier
-                            .size(180.dp)
-                            .clip(CircleShape),
-                        placeholder = painterResource(R.drawable.img_user_default_profile),
-                        error = painterResource(R.drawable.img_user_default_profile),
-                        contentScale = ContentScale.Crop,
+                VerticalSpacer(40)
+
+                UserInfoMenus(
+                    title = stringResource(R.string.user_info_pick_category_title),
+                    menus = listOf(
+                        MenuItem(
+                            imageVector = Icons.Outlined.Archive,
+                            contentDescription = stringResource(R.string.user_info_favorite_menu_icon_description),
+                            menuTitle = stringResource(R.string.user_info_favorite_menu_title),
+                            onMenuClick = { onFavoritePicksClick(userId) }
+                        ),
+                        MenuItem(
+                            imageVector = Icons.Default.MusicNote,
+                            contentDescription = stringResource(R.string.user_info_created_by_self_menu_icon_description),
+                            menuTitle = stringResource(R.string.user_info_created_by_self_menu_title),
+                            onMenuClick = { onMyPicksClick(userId) }
+                        )
                     )
+                )
 
-                    VerticalSpacer(40)
-
+                if (userId == userInfoViewModel.currentUser?.userId) {
                     UserInfoMenus(
-                        title = stringResource(R.string.user_info_pick_category_title),
+                        title = stringResource(R.string.user_info_setting_category_title),
                         menus = listOf(
                             MenuItem(
-                                imageVector = Icons.Outlined.Archive,
-                                contentDescription = stringResource(R.string.user_info_favorite_menu_icon_description),
-                                menuTitle = stringResource(R.string.user_info_favorite_menu_title),
-                                onMenuClick = { onFavoritePicksClick(userId) }
+                                imageVector = Icons.Outlined.SwitchAccount,
+                                contentDescription = stringResource(R.string.user_info_setting_profile_menu_icon_description),
+                                menuTitle = stringResource(R.string.user_info_setting_profile_menu_title),
+                                onMenuClick = onEditProfileClick
                             ),
                             MenuItem(
-                                imageVector = Icons.Default.MusicNote,
-                                contentDescription = stringResource(R.string.user_info_created_by_self_menu_icon_description),
-                                menuTitle = stringResource(R.string.user_info_created_by_self_menu_title),
-                                onMenuClick = { onMyPicksClick(userId) }
+                                imageVector = Icons.Outlined.Notifications,
+                                contentDescription = stringResource(R.string.user_info_setting_notification_menu_icon_description),
+                                menuTitle = stringResource(R.string.user_info_setting_notification_menu_title),
+                                onMenuClick = onEditNotificationClick
+                            ),
+                            MenuItem(
+                                imageVector = Icons.AutoMirrored.Outlined.Logout,
+                                contentDescription = stringResource(R.string.user_info_setting_sign_out_menu_icon_description),
+                                menuTitle = stringResource(R.string.user_info_setting_sign_out_menu_title),
+                                onMenuClick = { showLogOutDialog = true }
                             )
                         )
                     )
-
-                    if (userId == userInfoViewModel.currentUser?.userId) {
-                        UserInfoMenus(
-                            title = stringResource(R.string.user_info_setting_category_title),
-                            menus = listOf(
-                                MenuItem(
-                                    imageVector = Icons.Outlined.SwitchAccount,
-                                    contentDescription = stringResource(R.string.user_info_setting_profile_menu_icon_description),
-                                    menuTitle = stringResource(R.string.user_info_setting_profile_menu_title),
-                                    onMenuClick = onEditProfileClick
-                                ),
-                                MenuItem(
-                                    imageVector = Icons.Outlined.Notifications,
-                                    contentDescription = stringResource(R.string.user_info_setting_notification_menu_icon_description),
-                                    menuTitle = stringResource(R.string.user_info_setting_notification_menu_title),
-                                    onMenuClick = onEditNotificationClick
-                                ),
-                                MenuItem(
-                                    imageVector = Icons.AutoMirrored.Outlined.Logout,
-                                    contentDescription = stringResource(R.string.user_info_setting_sign_out_menu_icon_description),
-                                    menuTitle = stringResource(R.string.user_info_setting_sign_out_menu_title),
-                                    onMenuClick = { showLogOutDialog = true }
-                                )
-                            )
-                        )
-                    }
                 }
+            }
 
-                ExtendedFloatingActionButton(
-                    onClick = onBackToMapClick,
-                    modifier = Modifier
-                        .wrapContentWidth()
-                        .padding(horizontal = 8.dp)
-                        .padding(bottom = 48.dp)
-                        .align(Alignment.BottomCenter),
-                    shape = CircleShape,
-                    containerColor = Primary,
-                    contentColor = White,
+            ExtendedFloatingActionButton(
+                onClick = onBackToMapClick,
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .padding(horizontal = 8.dp)
+                    .padding(bottom = 48.dp)
+                    .align(Alignment.BottomCenter),
+                shape = CircleShape,
+                containerColor = Primary,
+                contentColor = White,
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Map,
+                    contentDescription = stringResource(R.string.user_info_icon_map_description),
+                    tint = White
+                )
+
+                HorizontalSpacer(8)
+
+                Text(
+                    text = stringResource(R.string.user_info_back_to_map_button_text),
+                    color = White,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+
+            if (showLogOutDialog) {
+                MessageAlertDialog(
+                    onDismissRequest = {
+                        showLogOutDialog = false
+                    },
+                    title = stringResource(R.string.sign_out_dialog_title),
+                    body = "",
+                    showBody = false
                 ) {
-                    Icon(
-                        imageVector = Icons.Outlined.Map,
-                        contentDescription = stringResource(R.string.user_info_icon_map_description),
-                        tint = White
+                    DialogTextButton(
+                        onClick = {
+                            showLogOutDialog = false
+                        },
+                        text = stringResource(R.string.sign_out_dialog_dismiss)
                     )
 
                     HorizontalSpacer(8)
 
-                    Text(
-                        text = stringResource(R.string.user_info_back_to_map_button_text),
-                        color = White,
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                }
-
-                if (showLogOutDialog) {
-                    MessageAlertDialog(
-                        onDismissRequest = {
+                    DialogTextButton(
+                        onClick = {
                             showLogOutDialog = false
+                            onSignOutClick()
                         },
-                        title = stringResource(R.string.sign_out_dialog_title),
-                        body = "",
-                        showBody = false
-                    ) {
-                        DialogTextButton(
-                            onClick = {
-                                showLogOutDialog = false
-                            },
-                            text = stringResource(R.string.sign_out_dialog_dismiss)
-                        )
-
-                        HorizontalSpacer(8)
-
-                        DialogTextButton(
-                            onClick = {
-                                showLogOutDialog = false
-                                onSignOutClick()
-                            },
-                            text = stringResource(R.string.sign_out_dialog_confirm),
-                            textColor = Primary,
-                            fontWeight = FontWeight.Bold
-                        )
-                    }
+                        text = stringResource(R.string.sign_out_dialog_confirm),
+                        textColor = Primary,
+                        fontWeight = FontWeight.Bold
+                    )
                 }
             }
         }
