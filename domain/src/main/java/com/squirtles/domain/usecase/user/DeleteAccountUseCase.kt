001cd4aabe8299e5e1e5c11eb265ff95aa00ae61
Ju Yungyeom <joo_930@naver.com>
2025-02-16 21:56:29 +0900

    [feature] 회원 탈퇴 시 좋아한 픽, 등록한 픽 삭제

--- domain/src/main/java/com/squirtles/domain/usecase/user/DeleteAccountUseCase.kt
+++ domain/src/main/java/com/squirtles/domain/usecase/user/DeleteAccountUseCase.kt
@@ -1,15 +1,50 @@
 package com.squirtles.domain.usecase.user
 
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.squirtles.domain.usecase.favorite.DeleteFavoriteUseCase
+import com.squirtles.domain.usecase.favorite.FetchFavoritePicksUseCase
+import com.squirtles.domain.usecase.mypick.DeletePickUseCase
+import com.squirtles.domain.usecase.mypick.FetchMyPicksUseCase
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
 
-class DeleteAccountUseCase @Inject constructor() {
-    operator fun invoke() {
+class DeleteAccountUseCase @Inject constructor(
+    private val fetchFavoritePicksUseCase: FetchFavoritePicksUseCase,
+    private val deleteFavoriteUseCase: DeleteFavoriteUseCase,
+    private val fetchMyPicksUseCase: FetchMyPicksUseCase,
+    private val deletePickUseCase: DeletePickUseCase
+) {
+    suspend operator fun invoke() = coroutineScope {
         val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            try {
+                // 1. 좋아한 픽 삭제
+                val favoritePicks = fetchFavoritePicksUseCase(currentUser.uid).getOrNull() ?: emptyList()
+                val favoritePicksDeleteJobs = favoritePicks.map {
+                    async { deleteFavoriteUseCase(it.id, currentUser.uid) }
+                }
 
-        // TODO DB에서 해당 유저와 관련된 모든 정보 지우기
+                // 2. 등록한 픽 삭제
+                val myPicks = fetchMyPicksUseCase(currentUser.uid).getOrNull() ?: emptyList()
+                val myPicksDeleteJobs = myPicks.map {
+                    async { deletePickUseCase(it.id, currentUser.uid) }
+                }
 
-        // 정상적으로 지운 후 유저 삭제
-        currentUser?.delete()
+                // 모든 삭제 작업이 끝날 때까지 기다림
+                (favoritePicksDeleteJobs + myPicksDeleteJobs).awaitAll()
+
+                // TODO
+                // 3. Firebase Firestore 유저 정보 삭제
+
+                // 4. Firebase Auth 유저 삭제
+                // currentUser.delete()
+            } catch (e: Exception) {
+                // 오류 처리
+                Log.e("DeleteAccount", "Error deleting user account: ${e.message}")
+            }
+        }
     }
 }
